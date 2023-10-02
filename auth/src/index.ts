0987ad8ea15985926af1eac0eb528697c96aa50f
agniadvani <advani.agni.agni@gmail.com>
2023-10-02 08:07:27 +0530

    Mongoose Typescript types defined

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import "express-async-errors"
+import mongoose from 'mongoose'
 import { currentUserRouter } from "./routes/current-user"
 import { signinRouter } from "./routes/signin"
 import { signoutRouter } from "./routes/signout"
@@ -21,6 +22,16 @@ app.all("*", async (req, res) => {
 
 app.use(errorHandler)
 
-app.listen(3000, () => {
-    console.log("Listening on 3000...")
-})
+const start = async () => {
+    try {
+        await mongoose.connect("mongodb://auth-mongo-srv:27017")
+        console.log("Connected to DB Successfully...")
+        app.listen(3000, () => {
+            console.log("Listening on 3000...")
+        })
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+start()
