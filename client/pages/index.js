3dcfeb1077362f8fc4831f3fddcd6cb48a83a293
agniadvani <advani.agni.agni@gmail.com>
2023-10-10 15:22:42 +0530

    Build Client feature added

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,15 @@
-const Index = () => {
+import BuildClient from "../api/build-client"
+
+
+const Index = ({ currentUser }) => {
+    console.log(currentUser)
     return <h1>Landing Page</h1>
+}
+
+Index.getInitialProps = async (context) => {
+    const client = BuildClient(context)
+    const { data } = await client.get("/api/users/currentuser").catch(err => console.log(err))
+    return data
 }
 
 export default Index
