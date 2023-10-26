45444a58e6537aa6565362a78f557eb07d77e157
agniadvani <advani.agni.agni@gmail.com>
2023-10-26 15:44:44 +0530

    Added Ticket Publish For Create Ticket

--- tickets/src/nats-wrapper.ts
+++ tickets/src/nats-wrapper.ts
@@ -3,14 +3,22 @@ import nats from "node-nats-streaming"
 class NatsWrapper {
     private _client?: nats.Stan
 
+    get client() {
+        if (!this._client) {
+            throw new Error('cannot access client before its initialization')
+        }
+
+        return this._client
+    }
+
     connect(clusterId: string, clientId: string, url: string): Promise<void> {
-        this._client = nats.connect(clientId, clientId, { url })
+        this._client = nats.connect(clusterId, clientId, { url })
         return new Promise((resolve, reject) => {
-            this._client!.on('connect', () => {
+            this.client.on('connect', () => {
                 console.log("Connected to NATS")
                 resolve()
             })
-            this._client!.on('error', (err) => {
+            this.client.on('error', (err) => {
                 reject(err)
             })
         })
