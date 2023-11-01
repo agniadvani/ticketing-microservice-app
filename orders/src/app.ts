c7534bb6237bc51a03e44ef958b8822b052364ee
agniadvani <advani.agni.agni@gmail.com>
2023-11-01 17:52:27 +0530

    Routes added for orders

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -2,10 +2,11 @@ import { currentUser, errorHandler, NotFoundError } from '@aggitix/common'
 import cookieSession from "cookie-session"
 import express from "express"
 import "express-async-errors"
-import { indexTicketRouter } from './routes'
-import { newTicketRouter } from "./routes/new"
-import { showTicketRouter } from './routes/show'
-import { updateTicketRouter } from './routes/update'
+import { indexOrderRouter } from '../routes'
+import { deleteOrderRouter } from '../routes/delete'
+import { newOrderRouter } from '../routes/new'
+import { showOrderRouter } from '../routes/show'
+
 
 const app = express()
 app.set('trust proxy', true)
@@ -17,10 +18,10 @@ app.use(cookieSession({
 
 app.use(currentUser)
 
-app.use(newTicketRouter)
-app.use(showTicketRouter)
-app.use(indexTicketRouter)
-app.use(updateTicketRouter)
+app.use(indexOrderRouter)
+app.use(showOrderRouter)
+app.use(newOrderRouter)
+app.use(deleteOrderRouter)
 
 app.all("*", async (req, res) => {
     throw new NotFoundError()
