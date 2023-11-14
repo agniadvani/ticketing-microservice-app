2f569bbbf7d8d683036ef2c5cd2e6445bb0e3c80
agniadvani <advani.agni.agni@gmail.com>
2023-11-14 21:21:47 +0530

    Added Delete order feature

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,21 @@
 import express from 'express'
+import { Order } from '../models/order'
+import { NotAuthorizedError, NotFoundError, OrderStatus } from '@aggitix/common'
 
 const router = express.Router()
 
 router.delete("/api/orders/:orderId", async (req: express.Request, res: express.Response) => {
-    res.send({})
+    const order = await Order.findById(req.params.orderId)
+    if(!order){
+        throw new NotFoundError()
+    }
+    if(order.userId !== req.currentUser!.id){
+        throw new NotAuthorizedError()
+    }
+
+    order.status = OrderStatus.Cancelled
+    await order.save()
+    return res.status(204).send(order)
 })
 
 export { router as deleteOrderRouter }
