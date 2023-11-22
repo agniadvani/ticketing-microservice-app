20ae716f0eef7ac110d19ee67332cf43e06103a7
Agni Advani <advani.agni.agni@gmail.com>
2023-11-23 04:47:33 +0530

    Added Order Cancelled Listener to tickets

--- tickets/src/events/listeners/__test__/order-cancelled-listener.test.ts
+++ tickets/src/events/listeners/__test__/order-cancelled-listener.test.ts
@@ -1,13 +1,13 @@
-import { OrderCreatedEvent, OrderStatus } from "@aggitix/common"
+import { OrderCancelledEvent, OrderCreatedEvent, OrderStatus } from "@aggitix/common"
 import { natsWrapper } from "../../../nats-wrapper"
-import { OrderCreatedListener } from "../order-created-listener"
 import mongoose from "mongoose"
 import { Ticket } from "../../../models/ticket"
 import { Message } from "node-nats-streaming"
+import { OrderCancelledListener } from "../order-cancelled-listener"
 
 const setup = async () => {
     // Create a Listener
-    const listener = new OrderCreatedListener(natsWrapper.client)
+    const listener = new OrderCancelledListener(natsWrapper.client)
 
     // Build a ticket
     const ticket = Ticket.build({
@@ -16,18 +16,15 @@ const setup = async () => {
         userId: "test_user"
     })
 
+    ticket.set({ orderId: new mongoose.Types.ObjectId().toHexString() })
     await ticket.save()
 
     // Define the data and msg for on message
-    const data: OrderCreatedEvent["data"] = {
+    const data: OrderCancelledEvent["data"] = {
         id: new mongoose.Types.ObjectId().toHexString(),
-        status: OrderStatus.Created,
-        userId: "test_user",
         version: 0,
-        expiresAt: "",
         ticket: {
             id: ticket.id,
-            price: 2000
         }
     }
 
@@ -47,7 +44,7 @@ const setup = async () => {
 it("acks the msg if there is a ticket for an order in the data recieved", async () => {
     const { ticket, data, msg } = await setup()
     const updatedTicket = await Ticket.findById(ticket.id)
-    expect(updatedTicket!.orderId).toEqual(data.id)
+    expect(updatedTicket!.orderId).toEqual(undefined)
     expect(msg.ack).toHaveBeenCalled()
 })
 
--- tickets/src/events/listeners/order-cancelled-listener.ts
+++ tickets/src/events/listeners/order-cancelled-listener.ts
@@ -1,26 +1,22 @@
-import { Listener, OrderCreatedEvent, Subjects } from "@aggitix/common";
+import { Listener, OrderCancelledEvent, Subjects } from "@aggitix/common";
 import { Message } from "node-nats-streaming";
 import { QUEUE_GROUP_NAME } from "./queue-group-name";
 import { Ticket } from "../../models/ticket";
 import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
 
-
-export class OrderCreatedListener extends Listener<OrderCreatedEvent>{
-    subject: Subjects.OrderCreated = Subjects.OrderCreated;
+export class OrderCancelledListener extends Listener<OrderCancelledEvent>{
+    subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
     queueGroupName = QUEUE_GROUP_NAME;
-    async onMessage(data: OrderCreatedEvent["data"], msg: Message): Promise<void> {
 
-        // Get the ticket for the order
+    async onMessage(data: OrderCancelledEvent["data"], msg: Message): Promise<void> {
         const ticket = await Ticket.findById(data.ticket.id)
 
-        // if no ticket return
         if (!ticket) {
-            console.log("No ticket found")
+            console.log("Ticket not found")
             return
         }
 
-        // set the orderId property for that ticket and save it
-        ticket.set({ orderId: data.id })
+        ticket.set({ orderId: undefined })
         await ticket.save()
 
         await new TicketUpdatedPublisher(this.client).publish({
@@ -32,7 +28,6 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent>{
             orderId: ticket.orderId
         })
 
-        // Ack the msg
         msg.ack()
     }
 
