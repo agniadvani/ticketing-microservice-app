99cd2cbc7fb2c610ad46ebb695c799d7232ad594
Agni Advani <advani.agni.agni@gmail.com>
2023-11-27 21:47:19 +0530

    Expiration Complete Event

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -7,8 +7,11 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent>{
     subject: Subjects.OrderCreated = Subjects.OrderCreated;
     queueGroupName = QUEUE_GROUP_NAME;
     async onMessage(data: OrderCreatedEvent["data"], msg: Message): Promise<void> {
+        const delay = new Date(data.expiresAt).getTime() - new Date().getTime()
         await expirationQueue.add({
             orderId: data.id
+        }, {
+            delay: delay
         })
 
         msg.ack()
