6899ed0ab37236f70b910ae9a364892c12c606a0
Agni Advani <advani.agni.agni@gmail.com>
2023-12-12 01:19:31 +0530

    Ticket add and display feature added

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -5,7 +5,7 @@ import Header from '../components/header'
 const App = ({ Component, pageProps, currentUser }) => {
     return <div>
         <Header currentUser={currentUser} />
-        <Component {...pageProps} />
+        <Component currentUser={currentUser} {...pageProps} />
     </div>
 }
 
@@ -18,7 +18,7 @@ App.getInitialProps = async appContext => {
     let pageProps = {}
 
     if (appContext.Component.getInitialProps) {
-        pageProps = await appContext.Component.getInitialProps(appContext.ctx)
+        pageProps = await appContext.Component.getInitialProps(appContext.ctx, client, data.currentUser)
     }
 
     console.log("appcomp:", pageProps)
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,33 @@
-import BuildClient from "../api/build-client"
+const LandingPage = ({ currentUser, tickets }) => {
+    const ticketList = tickets.map((ticket) => {
+        return (
+            <tr key={ticket.id}>
+                <td>{ticket.title}</td>
+                <td>{ticket.price}</td>
+            </tr>
+        );
+    });
 
+    return (
+        <div>
+            <h1>Tickets</h1>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>{ticketList}</tbody>
+            </table>
+        </div>
+    );
+};
 
-const Index = ({ currentUser }) => {
-    console.log(currentUser)
-    return currentUser ? <h1>You are signed in</h1> : <h1>You are not signed in</h1>
-}
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+    const { data } = await client.get('/api/tickets');
 
-Index.getInitialProps = async (context) => {
-    const client = BuildClient(context)
-    const { data } = await client.get("/api/users/currentuser").catch(err => console.log(err))
-    return data
-}
+    return { tickets: data };
+};
 
-export default Index
+export default LandingPage;
